ae89c6305fe7e9c744870d9938657de00a7d86fe
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-10-17 00:16:09 -0300

    feat: Implementação dos resultados em tela.

--- src/components/Pesquisar/index.js
+++ src/components/Pesquisar/index.js
@@ -25,6 +25,26 @@ const SubTitulo = styled.h3`
   margin-bottom: 40px;
 `;
 
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+
+  p {
+    width: 200px;
+  }
+
+  img {
+    width: 100px;
+  }
+
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
 function Pesquisa() {
   const [livrosPesquisa, setLivrosPesquisa] = useState([]);
 
@@ -36,14 +56,19 @@ function Pesquisa() {
         type="text"
         placeholder="Pesquisar"
         onBlur={(e) => {
-          const textoDigitado = e.target.value;
+          let textoDigitado = e.target.value;
           const resultadoPesquisa = livros.filter((livro) =>
             livro.nome.includes(textoDigitado)
           );
           setLivrosPesquisa(resultadoPesquisa);
         }}
       />
-      <p>{livrosPesquisa}</p>
+      {livrosPesquisa.map((livro) => (
+        <Resultado>
+          <p>{livro.nome}</p>
+          <img src={livro.src} alt="Livro" />
+        </Resultado>
+      ))}
     </ContainerPesquisar>
   );
 }
