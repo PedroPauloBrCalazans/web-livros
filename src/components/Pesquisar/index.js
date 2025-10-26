1daaa75928de87544030208f830e72dc52903d4e
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2025-10-26 15:33:45 -0300

    feat: Add assincronismo no useEffect.

--- src/components/Pesquisar/index.js
+++ src/components/Pesquisar/index.js
@@ -50,9 +50,13 @@ function Pesquisa() {
   const [livros, setLivros] = useState([]);
 
   useEffect(() => {
-    const livrosAPI = getLivros();
-    setLivros(livrosAPI);
+    fetchLivros();
   }, []);
+
+  async function fetchLivros() {
+    const livrosAPI = await getLivros();
+    setLivros(livrosAPI);
+  }
 
   return (
     <ContainerPesquisar>
