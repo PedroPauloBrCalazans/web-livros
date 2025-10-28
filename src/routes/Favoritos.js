efbb9e94da79a4ce6343005352149e06f32a6e02
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2025-10-28 15:54:21 -0300

    feat: Estilizando a page de favoritos.

--- src/routes/Favoritos.js
+++ src/routes/Favoritos.js
@@ -1,11 +1,46 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { getFavoritos } from "../api/favoritos";
+import livroImg from "../imagens/livro.png";
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+`;
+
+const ResultadoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+  cursor: pointer;
+  text-align: center;
+  padding: 0 100px;
+  p {
+    width: 200px;
+    color: #fff;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
+const Titulo = styled.h2`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+  padding-top: 35px;
 `;
 
 function Favoritos() {
@@ -22,9 +57,19 @@ function Favoritos() {
 
   return (
     <AppContainer>
-      {favoritos.map((favorito) => (
-        <p>{favorito.nome}</p>
-      ))}
+      <div>
+        <Titulo>Aqui estão seus livros favoritos:</Titulo>
+        <ResultadoContainer>
+          {favoritos.length !== 0
+            ? favoritos.map((favorito) => (
+                <Resultado>
+                  <p>{favorito.nome}</p>
+                  <img src={livroImg} alt="" />
+                </Resultado>
+              ))
+            : null}
+        </ResultadoContainer>
+      </div>
     </AppContainer>
   );
 }
